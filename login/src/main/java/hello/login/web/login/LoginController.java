e63a5591db7b5babb1157500dd99ceac1d3525cd
dongwook-han <dongook.han@oliveyoung.co.kr>
2026-03-10 23:07:00 +0900

    로그인 처리하기 - 쿠키 사용

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -3,6 +3,7 @@ package hello.login.web.login;
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import hello.login.web.session.SessionManager;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +27,7 @@ public class LoginController {
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
+    //    @PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -40,6 +41,22 @@ public class LoginController {
         }
 
         //로그인 성공 처리
+
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠기(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
